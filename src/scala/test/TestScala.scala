81976c9b3e2b44201b66341dbd7c7c4470fe24f8
edureka <edureka@localhost>
2016-05-23 00:55:49 +0530

    Factorial and gcd

--- src/scala/test/TestScala.scala
+++ src/scala/test/TestScala.scala
@@ -1,5 +1,24 @@
 package scala.test
 
 object TestScala {
+  
+  def factorial(num: Int): Int = {
+    
+    if(num <=1) 1
+    else num * factorial(num-1)
+  }
+  
+  def gcd(a: Int, b: Int): Int = {
+    
+    if(b == 0) a
+    else gcd(b, a%b)
+    
+  }
 
+  def main(args : Array[String]) {
+    
+    println(factorial(5))
+    println(gcd(11, 13))  
+  }
+  
 }
